0d4846173f43392eaf48ab618d31805a447890a9
Byron Marohn <combustible@live.com>
2018-03-23 18:31:20 -0700

    data logging cleanup
    Signed-off-by: Byron Marohn <combustible@live.com>

--- sketch_mar13a.ino
+++ sketch_mar13a.ino
@@ -6,16 +6,11 @@ const int TX_LED = PIN_LED_TXL; // TX LED on pin 26, we use the predefined PIN_L
 bool ledState = LOW;
 
 const int NUM_PADS = 5;
-#if 0
-float thresh_high[NUM_PADS] = {750, 1400, 1000, 1250, 1500};
-float thresh_low[NUM_PADS] =  {600, 1300,  900, 1100, 1400};
-#else
 float thresh_high[NUM_PADS] = {250, 600, 300, 400, 400};
 float thresh_low[NUM_PADS] =  {200, 500, 250, 300, 350};
-#endif
 uint8_t port_num[NUM_PADS] = {A3, A4, A1, A0, A2};
 int data[NUM_PADS];
-char *port_names[NUM_PADS] = {"Top Left", "Top Right", "Center", "Bottom Left", "Bottom Right"};
+char *port_names[NUM_PADS] = {"TL", "TR", "C", "BL", "BR"};
 char key[NUM_PADS] = {'q', 'e', 's', 'z', 'c'};
 bool state[NUM_PADS] = {false, false, false, false, false};
 
@@ -29,13 +24,7 @@ void setup()
 	digitalWrite(BLUE_LED, LOW);
 	analogReadResolution(12);
 
-#if 0
-	for (int i = 0; i < NUM_PADS; i++) {
-		pinMode(port_num[i], INPUT_PULLDOWN);
-	}
-#endif
-
-	SerialUSB.begin(9600);
+	SerialUSB.begin(115200);
 	Keyboard.begin();
 }
 
@@ -43,30 +32,26 @@ void loop()
 {
 	for (int i = 0; i < NUM_PADS; i++) {
 		data[i] = analogRead(port_num[i]);
-#if 0
-		SerialUSB.print(port_names[i]);
-		SerialUSB.print(" - ");
-		if (data[i] < thresh_low[i]) {
-			SerialUSB.print("___ - ");
-		} else if (data[i] > thresh_high[i]) {
-			SerialUSB.print("*** - ");
-		} else {
-			SerialUSB.print("MMM - ");
-		}
-		SerialUSB.println(data[i]);
-	}
 
-	int DELAY = 100;
-	digitalWrite(RX_LED, LOW); // RX LED on
-	delay(DELAY);
-	digitalWrite(RX_LED, HIGH); // RX LED off
-	digitalWrite(TX_LED, LOW); // TX LED on
-	delay(DELAY);
-	digitalWrite(TX_LED, HIGH); // TX LED off
-	digitalWrite(BLUE_LED, HIGH); // Blue LED on
-	delay(DELAY);
-	digitalWrite(BLUE_LED, LOW); // Blue LED off
-#else
+#if LOGGING
+		/* Print debugging info */
+		SerialUSB.print(port_names[i]);
+		SerialUSB.print(",");
+		SerialUSB.print(data[i]);
+		SerialUSB.print(",");
+		SerialUSB.print((int)state[i]);
+		if (data[i] < thresh_low[i]) {
+			SerialUSB.print("L");
+		} else if (data[i] > thresh_high[i]) {
+			SerialUSB.print("H");
+		} else {
+			SerialUSB.print("M");
+		}
+		SerialUSB.print(",");
+		SerialUSB.println((int)state[i]);
+#endif
+
+		/* Press keys */
 		if (data[i] < thresh_low[i] && state[i] == false) {
 			Keyboard.press(key[i]);
 			state[i] = true;
@@ -75,6 +60,5 @@ void loop()
 			state[i] = false;
 		}
 	}
-	delay(20);
-#endif
+	/*delay(20);*/
 }
